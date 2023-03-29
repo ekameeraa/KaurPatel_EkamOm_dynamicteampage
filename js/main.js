c116743fc83aff30b0c471d449b298c7add7a340
ekampreetkaur <113371633+ekameeraa@users.noreply.github.com>
2023-03-29 12:35:06 -0400

    updating
    ...

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let OM = document.querySelectorAll('.button'),
-    lightBox = document.querySelector('#lightbox')
-;
+    lightBox = document.querySelector('#lightbox'),
+    Ekam = document.querySelectorAll('.button');
 
 
 function loadMyData() {
@@ -10,4 +10,10 @@ function loadMyData() {
 
 }
 
+function loadMyData() {
+    lightBox.querySelector('h3').textContent = Ekam[this.dataset.member].headline;
+    lightBox.querySelector('p').textContent = Ekam[this.dataset.member].bio;
+}
+
+Ekam.forEach(hero => hero.addEventListener('click',loadMyData)),
 OM.forEach(hero => hero.addEventListener('click',loadMyData));
