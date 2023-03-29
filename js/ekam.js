62cca3c74ce1551f193a1d14689dbb8c4c240cd8
ekampreetkaur <113371633+ekameeraa@users.noreply.github.com>
2023-03-29 12:40:57 -0400

    adding js
    writing some javascript.

---
+++ js/ekam.js
@@ -0,0 +1,7 @@
+let me ={
+    Ekam: {
+        Name:'Ekampreet Kaur',
+        Headline:'DESIGNER',
+        Bio:' Design can be "Art". Design can be "Aesthetics". Design is so "Simple".That is why it is so "Complicated".'  
+    },
+}
